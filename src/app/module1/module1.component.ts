226533dc3dd09befabd61269063d6735815914be
Artem <aprricod@gmail.com>
2021-01-31 17:27:03 +0300

    add relations

---
+++ src/app/module1/module1.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-module1',
+  templateUrl: './module1.component.html',
+  styleUrls: ['./module1.component.css']
+})
+export class Module1Component implements OnInit {
+  param1: number = 10;
+  param2: boolean = true;
+  param3: string = 'hello';
+  param4: null = null;
+  param5: undefined = undefined;
+
+@Input()
+  inputParam6: string = '';
+
+@Output()
+  userChange = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  setValue(e: any): void {
+    this.param3 = e.target?.value||e.value;
+    this.userChange.emit(this.param3);
+  }
+
+}
